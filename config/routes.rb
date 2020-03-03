a0846198f0981bbc4f3dd4544acd4104715fa013
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-03-03 19:11:03 +0100

    FEAT - dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
     resources :reviews, only: %i[index create new]
   end
 
-  get 'dashboard', to: 'pages#dashboard'
+  # get 'dashboard', to: 'pages#dashboard'
+  resource :dashboard, only: [:show]
 
   resources :chats, only: [:index, :create] do
      resources :messages, only: [:index, :create]
