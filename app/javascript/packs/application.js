7c1341491d2d7cc55d4668db2031a637bca83834
Beatriz Arespacochaga <bearescu@gmail.com>
2020-03-05 12:37:04 +0100

    Price calculation in booking details made

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import './application.css'
 import '../components/message-form.js';
 import '../components/messages.js';
 import '../components/navbar.js';
+import '../components/price-calculation.js';
 
 import "controllers"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
