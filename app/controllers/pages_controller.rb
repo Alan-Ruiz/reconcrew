3f8df878e6fed0fc826c92c0121f2db12db7766e
Radovan Marjanovic <marjanovic.radovan@gmail.com>
2020-03-05 17:31:24 +0100

    navbars

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @categories = Category.all
+    # @locations = policy_scope(Location.all)
   end
 
   def profile
