0fc276c3e8aaaa41efde8fc6d7c86f1e4930f871
Alan Ruiz <gheryk90@gmail.com>
2020-03-05 12:21:49 +0100

    button disable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,14 @@ import './application.css'
 import '../components/message-form.js';
 import '../components/messages.js';
 import '../components/navbar.js';
+// import '../components/disable_button.js';
 
 import "controllers"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox.js';
+import {disableButton} from '../components/disable_button.js';
 
 initMapbox();
+
+disableButton();
