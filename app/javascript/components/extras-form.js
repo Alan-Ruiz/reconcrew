5f1a74ad6950ab51c936af53d6a39b52ba8a6c51
Beatriz Arespacochaga <bearescu@gmail.com>
2020-03-10 18:22:15 +0100

    minimal changes to show css

---
+++ app/javascript/components/extras-form.js
@@ -0,0 +1,7 @@
+const extrasSelect = document.getElementById("extras-button")
+
+  extrasSelect.addEventListener("click", (displayForm) => {
+    extrasSelect.classList.add('d-none')
+    const extrasForm = document.getElementById("extras-form")
+    extrasForm.classList.add('d-block')
+  });
