ddf2ebe7a33ab672f6a11a46a8b5f98006464953
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-03-11 12:16:32 +0100

    FIX - show page javascript

--- app/javascript/components/extras-form.js
+++ app/javascript/components/extras-form.js
@@ -1,5 +1,4 @@
 const extrasSelect = document.getElementById("extras-button")
-
   extrasSelect.addEventListener("click", (displayForm) => {
     extrasSelect.classList.add('d-none')
     const extrasForm = document.getElementById("extras-form")
