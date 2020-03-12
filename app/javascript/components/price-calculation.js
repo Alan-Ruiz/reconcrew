818a890d7eb9a308afd85cbbbdd088218b0d1855
Beatriz Arespacochaga <bearescu@gmail.com>
2020-03-12 11:37:13 +0100

    Total prices, and number of days will only show when a user selects one or more days.

--- app/javascript/components/price-calculation.js
+++ app/javascript/components/price-calculation.js
@@ -1,11 +1,19 @@
 const dateSelec = document.getElementById("date-selec")
+const totPriceSelect = document.getElementById('tot-price-days')
 
 if (dateSelec){
 
   dateSelec.addEventListener("change", (event) => {
   // const arrOfDates = event.target.value.split(', ');
   const numOfDays = event.target.value.split(', ').length;
-  let totalPriceDays = document.getElementById('total-price-days');
+  console.log(event.target.value)
+  if (event.target.value) {
+    totPriceSelect.classList.add('d-block');
+  } else {
+    totPriceSelect.classList.remove('d-block');
+  }
+
+  let totalPriceDays = document.querySelector('.total-price-days');
   let totalPrice = document.getElementById('total-price');
   const locationPrice = parseInt(document.getElementById('location-price').innerText);
   // total price takes the 0 from the hteml, and we want to asign it the number of days we got from flatpickr
@@ -13,3 +21,10 @@ if (dateSelec){
   totalPrice.innerText = numOfDays * locationPrice;
 });
 }
+
+
+// const totPriceSelect = document.getElementById('tot-price-days')
+
+// totPriceSelect.addEventListener('click', (displayTotalPrice) => {
+//   totPriceSelect.classList.add('d-block');
+// });
